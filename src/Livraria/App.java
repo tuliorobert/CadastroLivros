68df1e88bab075767b81a068c3427e8f93b4710a
Tulio <tuliorobert2@gmail.com>
2021-12-08 22:08:45 -0300

    Add files

---
+++ src/Livraria/App.java
@@ -0,0 +1,11 @@
+package Livraria;
+
+public class App {
+    public static void main(String[] args) {
+        Livraria livraria = new Livraria();
+
+        for (boolean rp = true; rp == true;) {
+            livraria.Menu();
+        }
+    }
+}
